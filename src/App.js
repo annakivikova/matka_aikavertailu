8f3dfd37fa1a1e89abcf9cd9f3792975faed7441
Anna Kivimäki-Kovacic <anna.a.kivimaki@gmail.com>
2018-07-08 16:04:09 +0300

    Changed the itinerary planning

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.onUpdate = this.onUpdate.bind(this);
+    this.onUpdateTo = this.onUpdateTo.bind(this);
     this.state = {
       updatedPos: '',
       updatedPosTo: '',
@@ -29,7 +30,7 @@ export class App extends Component {
   }
 
   render() {
-    console.log('updatedPos in component App is: ' + this.state.updatedPos);
+    console.log('updatedPos in component App is: ' + this.state.updatedPos + 'updatedPosTo in component App is: ' + this.state.updatedPosTo);
     return (
       <div>
         <h3>Lähtöosoite</h3>
--- src/Components/Routing.js
+++ src/Components/Routing.js
@@ -5,11 +5,23 @@ import gql from 'graphql-tag';
 const Routing = () => (
   <Query query={gql`
     {
-      stop(id: "HSL:1040129") {
-        name
-        lat
-        lon
-        wheelchairBoarding
+      plan(
+        from: {lat: 60.199196699999995, lon: 24.9397302}
+        to: {lat: 60.168438, lon: 24.929283}
+        numItineraries: 3
+        modes: "WALK,RAIL"
+      ) {
+        itineraries {
+          legs {
+            startTime
+            endTime
+            mode
+            duration
+            realTime
+            distance
+            transitLeg
+          }
+        }
       }
     }
   `}
@@ -21,7 +33,7 @@ const Routing = () => (
       return (
         <div>
           <p>
-            {`${data.stop.name}: ${data.stop.lat} ${data.stop.lon}`}
+            {`${data.plan.itineraries.legs}`}
           </p>
         </div>
       );
@@ -30,3 +42,24 @@ const Routing = () => (
 );
 
 export default Routing;
+
+/*stop(id: "HSL:1040129") {
+  name
+  lat
+  lon
+  wheelchairBoarding
+}
+}
+`}
+>
+{({loading, error, data}) => {
+if (loading) return <p>Loading...</p>;
+if (error) return <p>Error</p>;
+
+return (
+  <div>
+    <p>
+      {`${data.stop.name}: ${data.stop.lat} ${data.stop.lon}`}
+    </p>
+  </div>
+);*/
